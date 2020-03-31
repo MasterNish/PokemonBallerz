1c2395a6d9a95b5f24534f6f3fa6d08c04a895f0
Nishanth Muthukumar <nishanth.muthukumar@gmail.com>
2020-03-31 13:26:37 -0400

    Class
    Learned what a class is and got part way into creating one for the characters

--- Pokemon.py
+++ Pokemon.py
@@ -1,11 +1,45 @@
 from pygame import *
 from random import *
 from glob import *
-import Camera;
+
+class Character(sprite.Sprite):
+
+    def __init__(self, animations, x, y):
+        sprite.Sprite.__init__(self)
+
+        self.animations = animations
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.image = None
+        self.count = 0
+        self.anirate = 0
+        self.direction = "down"
+        self.speeds = {"walk": 0.45, "run": 0.75, "idle": 0}
+        self.action = "walk"
+
+    def animate(self):
+
+        self.anirate += 1
+        if self.anirate % 10 == 0:  # frame rate
+            self.count += 1
+        animation = self.animations[self.action]
+        self.image = animation[self.count % len(animation)]
+
+    def move(self):
+
+        if self.direction == "down":
+            self.rect.y += self.speeds[self.action]
+        if self.direction == "right":
+            self.rect.x += self.speeds[self.action]
+        if self.direction == "up":
+            self.rect.y -= self.speeds[self.action]
+        if self.direction == "left":
+            self.rect.x -= self.speeds[self.action]
+
 
 width, height = 800, 600
 screen = display.set_mode((width, height))
-
 running = True
 
 clock = time.Clock()
@@ -14,18 +48,26 @@ mp = image.load("maps/map.png")
 mp1 = image.load("maps/map1.png")
 map = transform.smoothscale(mp, (800, 600))
 
-
 direction = ["left", "right", "down", "up"]
-walk = {d: [transform.smoothscale(image.load(i), (18, 21)) for i in glob("sprites\\walking\\" + d + "\\*.png")] for d in direction}
-run = {d: [transform.smoothscale(image.load(i), (18, 21)) for i in glob("sprites\\running\\" + d + "\\*.png")] for d in direction}   #accesses all the pics from each direction folder and changes its size
+walk = {d: [transform.smoothscale(image.load(i), (18, 21)) for i in glob("sprites\\walking\\" + d + "\\*.png")] for d in
+        direction}
+run = {d: [transform.smoothscale(image.load(i), (18, 21)) for i in glob("sprites\\running\\" + d + "\\*.png")] for d in
+       direction}  # accesses all the pics from each direction folder and changes its size
 
-t1 = {d: [transform.smoothscale(image.load(i), (18, 21)) for i in glob("sprites\\t1\\" + d + "\\*.png")] for d in direction}
-t2 = {d: [transform.smoothscale(image.load(i), (18, 21)) for i in glob("sprites\\t2\\" + d + "\\*.png")] for d in direction}
-t3 = {d: [transform.smoothscale(image.load(i), (18, 21)) for i in glob("sprites\\t3\\" + d + "\\*.png")] for d in direction}
-t4 = {d: [transform.smoothscale(image.load(i), (18, 21)) for i in glob("sprites\\t4\\" + d + "\\*.png")] for d in direction}
-t5 = {d: [transform.smoothscale(image.load(i), (18, 21)) for i in glob("sprites\\t5\\" + d + "\\*.png")] for d in direction}
-t6 = {d: [transform.smoothscale(image.load(i), (18, 21)) for i in glob("sprites\\t6\\" + d + "\\*.png")] for d in direction}
-t7 = {d: [transform.smoothscale(image.load(i), (18, 21)) for i in glob("sprites\\t7\\" + d + "\\*.png")] for d in direction}
+t1 = {d: [transform.smoothscale(image.load(i), (18, 21)) for i in glob("sprites\\t1\\" + d + "\\*.png")] for d in
+      direction}
+t2 = {d: [transform.smoothscale(image.load(i), (18, 21)) for i in glob("sprites\\t2\\" + d + "\\*.png")] for d in
+      direction}
+t3 = {d: [transform.smoothscale(image.load(i), (18, 21)) for i in glob("sprites\\t3\\" + d + "\\*.png")] for d in
+      direction}
+t4 = {d: [transform.smoothscale(image.load(i), (18, 21)) for i in glob("sprites\\t4\\" + d + "\\*.png")] for d in
+      direction}
+t5 = {d: [transform.smoothscale(image.load(i), (18, 21)) for i in glob("sprites\\t5\\" + d + "\\*.png")] for d in
+      direction}
+t6 = {d: [transform.smoothscale(image.load(i), (18, 21)) for i in glob("sprites\\t6\\" + d + "\\*.png")] for d in
+      direction}
+t7 = {d: [transform.smoothscale(image.load(i), (18, 21)) for i in glob("sprites\\t7\\" + d + "\\*.png")] for d in
+      direction}
 
 # print(walk)
 # print(run)
@@ -38,7 +80,7 @@ posx, posy = 200, 200
 rs = 0.75
 ws = 0.45
 action = "walk"
-actions = {"walk": walk,"run": run}
+actions = {"walk": walk, "run": run}
 
 while running:
 
