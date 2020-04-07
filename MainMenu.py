9c72e4907fbfa1ebd4777de33a5c5b02cde9c844
Nishanth Muthukumar <nishanth.muthukumar@gmail.com>
2020-04-07 16:49:40 -0400

    stuff

--- MainMenu.py
+++ MainMenu.py
@@ -6,4 +6,8 @@ class MainMenu(sprite.Sprite) :
     def __init__(self, player):
         sprite.Sprite.__init__(self)
 
-        self.newFile = 
+        load = image.load("pics/white.jpg")
+        loadFile = transform.smoothscale(load, (300, 400))
+        loadSelect = Rect(100, 200, 300, 400)
+        if
+        self.newFile = draw.rect
